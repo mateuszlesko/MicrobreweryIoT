3a735458028ef889ff955594c7e6763c586d162e
mateuszlesko <mateusz.lesko@outlook.com>
2022-06-21 23:35:50 +0200

    RESTful CRUD

--- MicroBreweryMagazine/data/ingredients.go
+++ MicroBreweryMagazine/data/ingredients.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -18,13 +19,66 @@ type Ingredient struct {
 
 type Ingredients []*Ingredient
 
+var NotFoundError = fmt.Errorf("NOT FOUND RESOURCE")
+
+func (i *Ingredient) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(i)
+}
 func (i *Ingredients) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(i) //returns error
 }
 
+func (i *Ingredient) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(i) // pass reference to myself, map json to struct
+}
+
 func GetIngredients() Ingredients {
 	return ingredientList
+}
+
+func AddIngredient(i *Ingredient) {
+	i.ID = getNextID()
+	ingredientList = append(ingredientList, i)
+}
+
+func UpdateIngredient(id int, i *Ingredient) error {
+	_, index, err := findIngredient(id)
+	if err != nil && index < 0 {
+		return err
+	}
+
+	i.ID = id
+	ingredientList[index] = i
+	return nil
+}
+
+func RemoveIngredient(id int) error {
+	_, index, err := findIngredient(id)
+	if err != nil && index < 0 {
+		return err
+	}
+	ingredientList[index] = nil
+	return nil
+}
+
+func FindIngredient(id int) (*Ingredient, int, error) {
+	return findIngredient(id)
+}
+
+func getNextID() int {
+	return ingredientList[len(ingredientList)-1].ID + 1
+}
+
+func findIngredient(id int) (*Ingredient, int, error) {
+	for i, ingredient := range ingredientList {
+		if ingredient.ID == id {
+			return ingredient, i, NotFoundError
+		}
+	}
+	return nil, -1, fmt.Errorf("RESOURCE NOT FOUND")
 }
 
 var ingredientList = []*Ingredient{
